d03262f30c547e0bbae6438ccb3866dd1ccce6a8
ashleyyy <ashleyfisher@gmail.com>
2016-02-18 13:40:13 -0800

    added job model and create method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,32 @@
 class JobsController < ApplicationController
+  def index
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
   def new
   end
+
+  def edit
+  end
+
+  def create
+     @job = Job.new(strong_params)
+ 
+    @job.save
+    redirect_to @job
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def strong_params
+      params.require(:job).permit(:title, :company, :location, :description)
+    end
 end
